1d0f52f53b6da1c2fa5f1134d3017046addb2af3
Oriojas <oscarriojas@gmail.com>
2024-09-09 19:49:34 -0500

    Se crea primer contrato en Avalanche

---
+++ Hola_Mundo.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// Mi primer contrato hola mundo
+
+// 0xeF177F81D1980fd6DccAb5C58dAf0D7be6aE9373
+// 0x8b952F1770aAD3DDf98F11eBc539D2f0A423a861
+// 0xd07e9a02443cAB15CbBd2db119Db5e02A6F17971
+
+pragma solidity 0.8.24;
+
+contract hola_mundo {
+    string public saludo = "Hola mundo desde Avalanche Colombia";
+}
