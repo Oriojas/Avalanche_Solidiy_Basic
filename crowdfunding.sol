0c631e9daf56d3d08f481dbcd87a3ca628a468bb
Oriojas <oscarriojas@gmail.com>
2024-10-06 18:26:08 -0500

    se soluciona bug en contrato

--- crowdfunding.sol
+++ crowdfunding.sol
@@ -2,13 +2,12 @@
 
 pragma solidity 0.8.24;
 
-contract crowdFunding {
-
-    mapping(address => uint) public fundContributors; 
+contract CrowdFunding {
+    mapping(address => uint) public fundContributors; // dinero que se va a donar
     address public manager; // administrador del contrato
-    uint public deadline; // tiempo de vigencia de la campana
-    uint public targetAmount; // objetivo de la campana
-    uint public reaisedAmount; // cantidad recaudada
+    uint public deadline; // tiempo de campana
+    uint public targetAmount; // objetivo de la camapana
+    uint public raisedAmount; // dinero realmente recogido
 
     struct Request {
         string description;
@@ -17,62 +16,60 @@ contract crowdFunding {
         bool completed;
     }
 
-    mapping (uint => Request) public requests;
-    uint public numRequets;
+    mapping(uint => Request) public requests;
+    uint public numRequests;
 
     constructor(uint _targetAmount, uint _deadline) {
         targetAmount = _targetAmount;
-        deadline = _deadline;
         deadline = block.timestamp + _deadline;
         manager = msg.sender;
     }
 
-    // esta funcion nos permite enviar los fondos
+    // esta funcion me permite hacer la donacion a mi contrato
     function sendFunds() public payable {
-        require(block.timestamp < deadline, "Termino el deadline");
+        require(block.timestamp < deadline, "El plazo ha terminado");
         fundContributors[msg.sender] += msg.value;
-        reaisedAmount += msg.value;
+        raisedAmount += msg.value;
     }
 
-    // esta funcion nos retorna el valor recaudado
+    // consultar el balance de el contrato
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
 
-    // funcion de reembolso
+    // transferir los fondos del contrato una vez se cumplan las condiciones de tiempo y valor
     function refund() public  {
-        require(block.timestamp > deadline && reaisedAmount < targetAmount, "No se cumplio el objetivo, te regresamos el dinero");
+        require(block.timestamp > deadline && raisedAmount < targetAmount, "La meta fue alcanzada o el plazo no ha terminado");
         uint amountToRefund = fundContributors[msg.sender];
-        require(amountToRefund > 0, "No pusiste para la vaca");
+        require(amountToRefund > 0, "No has contribuido fondos");
         fundContributors[msg.sender] = 0;
+        payable(msg.sender).transfer(amountToRefund);
     }
 
-    //para que solo el administrador del contrato retire los fondos
-    modifier onlyManager () {
-        require(msg.sender == manager, "Solo el administrador del contrato puede retorar los fondos");
+    // me permite que solo la persona que desplego el contrato pueda ejecutar esas funciones
+    modifier onlyManager() {
+        require(msg.sender == manager, "Solo el administrador puede realizar esta accion");
         _;
     }
 
-    // funcion para enviar los fondos
-    function createRequest(string memory _description, address payable _recipient, uint _value) public {
-        Request storage newRequest = requests[numRequets];
-        numRequets ++;
+    // si se cumplio el objetivo y el que lo ejecuta es el manager puedo sacar dinero del contrato a un tercero
+    function createRequest(string memory _description, address payable _recipient, uint _value) public onlyManager {
+        require(raisedAmount >= _value, "No hay fondos suficientes para esta solicitud");
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
         newRequest.description = _description;
         newRequest.recipient = _recipient;
         newRequest.value = _value;
         newRequest.completed = false;
-
     }
 
-    // funcion para hacer los pagos
-    function makePayament(uint _requestNum) public onlyManager{
-        require(reaisedAmount >= targetAmount, "No se cumplio la meta");
+    // esta funcion me permite hacer el pago de la peticion anterior
+    function makePayment(uint _requestNum) public onlyManager {
+        require(raisedAmount >= targetAmount, "No se cumplio la meta");
         Request storage thisRequest = requests[_requestNum];
-        require(thisRequest.completed == false, "Esta peticion no fue completada");
+        require(!thisRequest.completed, "Esta peticion ya fue completada");
         thisRequest.recipient.transfer(thisRequest.value);
-
         thisRequest.completed = true;
     }
-
 
 }
